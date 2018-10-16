b8525ced866994c94fe2899a4c634b1bdb0ed4a0
kubiakba <kubiakb1@gmail.com>
2018-10-16 22:22:58 +0200

    add contract calculator

---
+++ src/main/java/pl/bk/contract/domain/calculator/ContractCalculator.java
@@ -0,0 +1,101 @@
+package pl.bk.contract.domain.calculator;
+
+import lombok.AllArgsConstructor;
+import org.javamoney.moneta.Money;
+import org.springframework.stereotype.Service;
+import pl.bk.contract.api.dto.ContractDto;
+import pl.bk.contract.domain.tax.fixed.FixedCostsTax;
+import pl.bk.contract.domain.tax.income.CountryDailyIncomeTax;
+
+import javax.money.CurrencyUnit;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Set;
+
+import static java.util.Arrays.stream;
+import static java.util.Currency.getInstance;
+import static java.util.stream.Stream.of;
+import static javax.money.Monetary.getCurrency;
+import static pl.bk.contract.domain.validator.ContractValidator.isCountryISOValid;
+import static pl.bk.contract.domain.validator.DailyIncomeTaxValidator.isCalculationForCountrySupported;
+import static pl.bk.contract.domain.validator.FixedCostsTaxValidator.isFixedCostsDefinedForCountry;
+
+@Service
+@AllArgsConstructor
+public class ContractCalculator
+{
+    private final Set<CountryDailyIncomeTax> countriesDailyIncomeTax;
+    private final long NUMBER_OF_DAYS_IN_MONTH = 22;
+    
+    public Money calculateDailyNetIncome(ContractDto contract)
+    {
+        isCountryISOValid(contract.getISOCountry());
+        final CurrencyUnit unit = getCurrencyUnit(contract);
+        
+        final Money dailyGrossEarnings = Money.of(new BigDecimal(contract.getDailyGrossEarnings()), unit);
+        
+        isCalculationForCountrySupported(countriesDailyIncomeTax, contract);
+        
+        final Money dailyGrossMinusIncomeTax = applyDailyIncomeTax(contract, dailyGrossEarnings);
+        
+        final Money monthEarnings = calculateMonthEarnings(dailyGrossMinusIncomeTax);
+        
+        final Money monthEarningMinusFixCosts = substractFixCosts(contract, monthEarnings);
+        
+        final Money dailyNetEarnings = calculateDailyNetEarnings(monthEarningMinusFixCosts);
+        
+        return dailyNetEarnings;
+    }
+    
+    private Money calculateDailyNetEarnings(Money monthEarningMinusFixCosts)
+    {
+        return monthEarningMinusFixCosts.divide(NUMBER_OF_DAYS_IN_MONTH);
+    }
+    
+    private Money substractFixCosts(ContractDto contract, Money monthEarnings)
+    {
+        final FixedCostsTax costs = getFixCostsForCountry(contract.getISOCountry());
+        return monthEarnings.subtract(costs.getMoney());
+    }
+    
+    private FixedCostsTax getFixCostsForCountry(String isoCountry)
+    {
+        isFixedCostsDefinedForCountry(isoCountry);
+        return stream(FixedCostsTax.values())
+            .filter(it -> isoCountry.equals(it.name()))
+            .findFirst()
+            .get();
+    }
+    
+    private Money calculateMonthEarnings(Money moneyMinusIncomeTax)
+    {
+        return moneyMinusIncomeTax.multiply(NUMBER_OF_DAYS_IN_MONTH);
+    }
+    
+    private Money applyDailyIncomeTax(ContractDto contract, Money dailyEarnings)
+    {
+        return of(dailyEarnings)
+            .map(money ->
+                     countriesDailyIncomeTax
+                         .stream()
+                         .filter(country -> country.getISOCountry().equals(contract.getISOCountry()))
+                         .findFirst()
+                         .get()
+                         .calculate(money))
+            .findFirst()
+            .get();
+    }
+    
+    private CurrencyUnit getCurrencyUnit(ContractDto contract)
+    {
+        final Currency currency = getCurrencyFrom(contract);
+        return getCurrency(currency.getCurrencyCode());
+    }
+    
+    private Currency getCurrencyFrom(ContractDto contract)
+    {
+        return getInstance(new Locale(contract.getISOCountry()));
+    }
+    
+}
