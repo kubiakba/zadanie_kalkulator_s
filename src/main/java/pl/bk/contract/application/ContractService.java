235bd7811c14fe6159ee62ac109e533c1e91d1e4
kubiakba <kubiakb1@gmail.com>
2018-10-17 20:08:27 +0200

    add rounding

--- src/main/java/pl/bk/contract/application/ContractService.java
+++ src/main/java/pl/bk/contract/application/ContractService.java
@@ -7,6 +7,8 @@ import pl.bk.contract.api.dto.ContractDto;
 import pl.bk.contract.domain.calculator.ContractCalculator;
 import pl.bk.contract.domain.exchange.CurrencyUnitExchanger;
 
+import javax.money.Monetary;
+
 @Service
 @AllArgsConstructor
 public class ContractService
@@ -17,7 +19,7 @@ public class ContractService
     public ContractDto calculateDailyNetEarningsInPLN(ContractDto contractDto)
     {
         final Money dailyNetEarnings = contractCalculator.calculateDailyNetEarnings(contractDto);
-        final Money money = currencyUnitExchanger.convertMoneyToPLN(dailyNetEarnings);
+        final Money money = currencyUnitExchanger.convertMoneyToPLN(dailyNetEarnings).with(Monetary.getDefaultRounding());
         return new ContractDto("PL", money.getNumber().toString());
     }
 }
