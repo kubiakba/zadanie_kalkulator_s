08421299d60e2e544aca0bd6fdf6a059491bc79d
kubiakba <kubiakb1@gmail.com>
2018-10-17 12:06:31 +0200

    add configuration beans

---
+++ src/main/java/pl/bk/contract/Config.java
@@ -0,0 +1,22 @@
+package pl.bk.contract;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config
+{
+    @Bean
+    public RestTemplate template()
+    {
+        return new RestTemplate();
+    }
+    
+    @Bean
+    public ObjectMapper objectMapper()
+    {
+        return new ObjectMapper();
+    }
+}
