cf48c98d944eb2d86b650eb7f0accbb910189df4
kubiakba <kubiakb1@gmail.com>
2018-10-17 20:08:41 +0200

    add modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContractComponent } from './contract/contract.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,7 @@ import { ContractComponent } from './contract/contract.component';
     ContractComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
